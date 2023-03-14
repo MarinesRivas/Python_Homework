e5ccc2d089806ee6382ba9678c72f6f018b29d95
Marines Rivas <mrivas@uaz.edu.mx>
2023-03-14 12:48:21 -0600

    create ejercicio_1.py

---
+++ 00_Introduccion/ejercicio_1.py
@@ -0,0 +1,33 @@
+#MARIA INES RIVAS RECENDEZ
+
+#Ejercicio 1
+
+# Introducción
+
+print(5 + 5)                       # 10
+print(7 - 2)                       # 5
+print(2 * 7)                       # 14
+print(9 / 3)                       # 3
+print(5 ** 5)                      # 3125
+print(9 % 8)                       # 1
+print(1 // 2)                      # 3
+print(10<8)                        # False
+print(5.5 * 2.55)                  # 14.025
+print(3j + 5 + 1j + 2)             # (7+4j)
+print('Python')                    # Python
+print(['lol', 5, 7])               # ['lol', 5, 7]
+print({'curso':'Python'})          # {'curso': 'Python'}
+print({'lol', 5.555, 3.01})        # {5.555, 3.01, 'lol'}
+print(('lol', 3.33, 2.22))          # ('lol', 3.33, 2.22  )
+
+# Tipos de datos
+
+print(type(10<8))                # bool // Buleano
+print(type(5 + 5))               # int /Entero
+print(type(5.5 * 2.55))          # float // Flotante
+print(type(3j + 5 + 1j + 2))     # Complex // Complejo
+print(type('Python'))            # String // Cadena
+print((['lol', 5, 7]))           # List // Lista
+print(type({'curso':'Python'}))  # Dictionary // Diccionario
+print(type({'lol', 5.555, 3.01}))    # Set // Conjunto
+print(type(('lol', 3.33, 2.22)))    # Tuple // Tupla
