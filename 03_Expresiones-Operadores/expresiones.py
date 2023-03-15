e68be3154047651ca8012b6bef9bc2eaaf77e296
Marines Rivas <mrivas@uaz.edu.mx>
2023-03-15 17:51:38 -0600

    update

--- 03_Expresiones-Operadores/expresiones.py
+++ 03_Expresiones-Operadores/expresiones.py
@@ -66,7 +66,8 @@ b = 2
 c = a % b
 print(c)
 print(6%3)
-Operador **.
+
+#Operador **.
 a = 3 ** 3
 print(a)
 a = 2
@@ -74,6 +75,9 @@ b = 4
 c = a ** b
 print(c)
 print(4**3)
+
+"""
+
 Expresiones relacionales
 
 < - Este es el operador de menor que, y se utiliza en las expresiones relacionales para saber si un operando es menor a otro operando.
@@ -86,7 +90,10 @@ NOTA: Como resultado de la aplicación de las expresiones relacionales, se obten
 
 A continuación se presentan algunos ejemplos de las expresiones relacionales en código Python.
 
-Operador <.
+
+"""
+
+#Operador <.
 a = 3 < 3
 print(a)
 a = 2
@@ -94,7 +101,8 @@ b = 4
 c = a < b
 print(c)
 print(4<3)
-Operador >.
+
+#Operador >.
 a = 4 > 2
 print(a)
 a = 5
@@ -102,7 +110,8 @@ b = 7
 c = a > b
 print(c)
 print(9>2)
-Operador ==.
+
+#Operador ==.
 a = 5 == 5
 print(a)
 a = 6
@@ -110,7 +119,8 @@ b = 9
 c = a == b
 print(c)
 print(6==2)
-Operador !=.
+
+#Operador !=.
 a = 4 != 2
 print(a)
 a = 5
@@ -118,7 +128,8 @@ b = 3
 c = a != b
 print(c)
 print(8!=8)
-Operador <=.
+
+#Operador <=.
 a = 5 <= 3
 print(a)
 x = 7
@@ -126,7 +137,8 @@ y = 5
 z = x <= y
 print(z)
 print(9<=4)
-Operador >=.
+
+#Operador >=.
 a = 2 >= 8
 print(a)
 a = 3
@@ -134,25 +146,44 @@ b = 4
 c = a >= b
 print(c)
 print(7>=3)
+
+
+"""""
 Expresiones lógicas.
 not - Este es el operador lógico para realizar una negación, si el operando no se encuentra en el otro operando, la expresión se cumple y resulta en True, caso contrario, si la expresión no se cumple el resultado es False.
 and - Este es el operador lógico para realizar una conjugación, si cualquiera de los operandos presentes no se cumple en la expresión lógica, esta resulta en False, caso contrario, si toda la expresión lógica de conjugación se cumple el resultado es True.
 or - Este es el operador lógico para realizar una conjugación, si cualquiera de los operandos presentes no se cumple en la expresión lógica, esta resulta en False, caso contrario, si toda la expresión lógica de conjugación se cumple el resultado es True.
-Operador and.
+
+
+"""
+
+#Operador and.
 print(4-1==3 and 5>6)
 print(6+7 > 11 and 3==3)
-Operador or.
+
+#Operador or.
 print(4-1==3 or 5>6)
 print(6+7 > 11 or 3==3)
-Operador not.
+
+#Operador not.
 print(not 5>6)
 print(not 5>4)
+
+
+
+
+"""
+
+
 Expresiones de carácter
 A diferencia de las demás expresiones no existe un operador estático sino una búsqueda de secuencias, números o caracteres dentro de una variable.
 
 Estas expresiones de búsqueda comúnmente llamadas expresiones regulares, sirven para captar ciertos elementos o patrones dentro de un valor.
 
-Ejemplo:
+
+"""
+
+#Ejemplo:
 
 import re
 frase = "Tengo 2 hijos que tienen 15 y 11 años"
