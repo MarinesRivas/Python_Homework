9de61a6aa2cfdf26472995454c62463af90c9e06
Marines Rivas <mrivas@uaz.edu.mx>
2023-03-15 18:16:53 -0600

    update

--- 03_Expresiones-Operadores/expresiones.py
+++ 03_Expresiones-Operadores/expresiones.py
@@ -189,17 +189,4 @@ import re
 frase = "Tengo 2 hijos que tienen 15 y 11 años"
 patron = '[0-9]+' #Esta es una expresión regular
 re.findall(patron, frase)
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
