60faae598e73a2d18ea728eb934654c2e9bc270d
Marines Rivas <mrivas@uaz.edu.mx>
2023-03-27 16:32:07 -0600

    update

---
+++ 08_Ciclos/ejercicio_ciclos.py
@@ -0,0 +1,12 @@
+
+#MARIA INES RIVAS RECEDNEZ#
+
+"""
+CICLOS
+"""
+
+numeros = [0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15]
+i=0
+for i in range(i,len(numeros)): # el número es un nombre temporal para referirse a los elementos de la lista, válido solo dentro de este ciclo
+    print(i)       # los números se imprimirán línea por línea, del 0 al 5
+
