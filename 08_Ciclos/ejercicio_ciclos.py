02f59763d1bc489d0ae87279608dd722d4dc8195
Marines Rivas <mrivas@uaz.edu.mx>
2023-03-27 16:52:14 -0600

    update

--- 08_Ciclos/ejercicio_ciclos.py
+++ 08_Ciclos/ejercicio_ciclos.py
@@ -10,3 +10,39 @@ i=0
 for i in range(i,len(numeros)): # el número es un nombre temporal para referirse a los elementos de la lista, válido solo dentro de este ciclo
     print(i)       # los números se imprimirán línea por línea, del 0 al 5
 
+print(i+1) # numeros en linea mas el siguiente termino en este caso el 16
+
+for i in range (i,len(numero), 2):
+
+    print(i)
+
+#diccionario#
+
+    persona = {
+    'nombre':'Marines',
+    'apellido':'Rivas',
+    'edad':29,
+    'pais':'México',
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+
+    }}
+
+    print('###################')
+    for llave in persona:
+        if llave=='skills':
+            for skill in persona['skills']:
+                print(skill)
+
+break
+else:
+
+print("no esta")
+
+
+
+
+
+
