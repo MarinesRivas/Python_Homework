bdec991f073257410317904ae26f23d9c35eda5f
Marines Rivas <mrivas@uaz.edu.mx>
2023-04-19 13:46:05 -0600

    update

--- TAREAS/Tarea_4.py
+++ TAREAS/Tarea_4.py
@@ -3,3 +3,51 @@
 
 #TAREA 4
 
+'''''
+
+if number > 0:
+    print("Positive number")
+@@ -10,7 +10,6 @@
+
+print('This statement is always executed')
+
+# Un ejemplo de este seria
+#number = 5
+
+# outer if statement
+if (number >= 0):
+    # inner if statement
+    if number == 0:
+      print('Number is 0')
+    
+    # inner else statement
+    else:
+        print('Number is positive')
+# outer else statement
+else:
+    print('Number is negative')
+# Output: Number is positive
+
+
+a = 100
+if a > 0:
+    print('A es un número positivo') # a es un número positivo
+
+
+a = 5
+if a < 6:
+    print('a es un número negativo')
+else:
+    print('a es un número positivo')  #a es un número negativo
+
+
+a = -10
+if a > 0:
+    print('A es un número positivo')
+elif a < 0:
+    print('A es un número negativo')
+else:
+    print('A es cero')  # a es un número negativo
+
+'''
+#no me sale :(
