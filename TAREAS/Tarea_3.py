068f687a26cd21596a1fe27fa8b8ae324c4548c1
Marines Rivas <mrivas@uaz.edu.mx>
2023-04-19 13:10:57 -0600

    update

--- TAREAS/Tarea_3.py
+++ TAREAS/Tarea_3.py
@@ -3,3 +3,21 @@
 
 #TAREA 3
 
+#LISTAS
+
+Mi_lista = [] # no imprime nada
+
+lista = [1, ['a', 'b', 'c', 'd', 'e'], 2, 5.5, 'HolaMundo'] 
+
+print (lista) # imprime  [1, ['a', 'b', 'c', 'd', 'e'], 2, 5.5, 'HolaMundo'], Distintos tipos de elementos
+
+lista.reverse()
+print(lista)  # imprime ['HolaMundo', 5.5, 2, ['a', 'b', 'c', 'd', 'e'], 1]
+
+lista=[10, 20, 30]
+
+print(len(lista))    # imprime un 3
+lista.append(100)
+print(len(lista))    # imprime un 4
+print(lista[0])      # imprime un 10
+print(lista[3])      # imprime un 100
